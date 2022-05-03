85e80ea78d2b0a77320fa0461198db68d4736482
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-05-03 14:45:10 +0300

    [add] added class Road

--- src/classes.js
+++ src/classes.js
@@ -1,2 +1,21 @@
 'use strict';
 
+class Road  {
+  constructor(image, y) {
+    this.x = 0;
+    this.y = y;
+    this.loaded = false;
+    this.image = new Image();
+    let obj = this;
+    this.image.addEventListener('load', () => obj.loaded = true);
+    this.image.src = image;
+  }
+
+  update(road) {
+    this.y += gameSpeed;
+    if (this.y > window.innerHeight) {
+      this.y = road.y - canvas.width + gameSpeed; 
+    }
+  }
+}
+
