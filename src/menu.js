4aa21d0768828b1461aa89ef31e457da0f31bfd3
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-05-06 15:49:49 +0300

    [add] added MenuScene class

--- src/menu.js
+++ src/menu.js
@@ -16,7 +16,7 @@ window.Game = class {
     document.addEventListener('keyup', e => { this.keys[e.which] = false; });
   }
   checkKeyPress(keyCode) {
-    let isKeyPressed = this.keys[keyCode];
+    const isKeyPressed = this.keys[keyCode];
     this.lastKeyState = this.lastKeyState || {};
     if (typeof this.lastKeyState[keyCode] === 'undefined') {
       this.lastKeyState[keyCode] = isKeyPressed;
@@ -98,6 +98,74 @@ window.IntroScene = class {
       ctx.fillStyle = '#000';
       ctx.fillText(this.infoText.substr(0, Math.floor(this.infoText.length * textProgress)),
         (canvas.width - ctx.measureText(this.infoText).width) / 2, canvas.height / 2 + 80);
+    }
+  }
+};
+
+// Menu scene
+window.MenuScene = class {
+  constructor(game) {
+    // set default values
+    this.game = game;
+    this.opacityDirection = 1;
+    this.menuActiveOpacity = 0;
+    this.menuIndex = 0;
+    this.menuTitle = 'Game Menu';
+    this.menuItems = [
+      'Start',
+      'Garage',
+      'Exit'
+    ];
+  }
+  update(dt) {
+    // calculate active menu item opacity
+    const opacityValue = this.menuActiveOpacity + dt * this.opacityDirection;
+    if (opacityValue > 1 || opacityValue < 0) this.opacityDirection *= -1;
+    this.menuActiveOpacity += dt * this.opacityDirection;
+
+    // menu navigation
+    if (this.game.checkKeyPress(83)) { // DOWN arrow
+      this.menuIndex++;
+      this.menuIndex %= this.menuItems.length;
+    } else if (this.game.checkKeyPress(87)) { // UP arrow
+      this.menuIndex--;
+      if (this.menuIndex < 0) this.menuIndex = this.menuItems.length -1;
+    }
+
+    // menu item selected
+    if (this.game.checkKeyPress(13)) {
+      switch (this.menuIndex) {
+      case 0: this.game.setScene(GameScene); break;
+      case 1: this.game.setScene(Garage); break;
+      case 2: this.game.setScene(ExitScene); break;
+      }
+    }
+  }
+  render(dt, ctx, canvas) {
+    //fill menu background
+    const backgroundImage = new Image();
+    backgroundImage.src = './images/menuBackground.png';
+    ctx.fillStyle = '#c0c0c0';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, 0, 0);
+    // draw menu title
+    ctx.font = '60px Comic Sans MS';
+    ctx.textBaseline = 'top';
+    ctx.fillStyle = '#00ffff';
+    ctx.fillText(this.menuTitle, (canvas.width - ctx.measureText(this.menuTitle).width) / 2, 10);
+
+    // draw menu items
+    const itemHeight = 50, fontSize = 30;
+    ctx.font = fontSize + 'px Comic Sans MS';
+    for (const [index, item] of this.menuItems.entries()) {
+      if (index === this.menuIndex) {
+        ctx.globalAlpha = this.menuActiveOpacity;
+        ctx.fillStyle = '#089cd3';
+        ctx.fillRect(0, canvas.height / 2 + index * itemHeight, canvas.width, itemHeight);
+      }
+      ctx.globalAlpha = 1;
+      ctx.fillStyle = '#fff';
+      ctx.fillText(item, (canvas.width - ctx.measureText(item).width) / 2, canvas.height / 2 + index * itemHeight + (itemHeight - fontSize) / 2);
     }
   }
 };
